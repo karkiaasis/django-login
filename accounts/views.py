f91b28087aca8cc67c3a444046bb226c1a559663
karkiaasis <info.aasiskarki@gmail.com>
2020-06-13 11:14:16 +0545

    register and login forms with views created

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.contrib.auth import (
+    authenticate,
+    get_user_model,
+    login,
+    logout
+)
+
+from .forms import UserLoginForm, UserRegisterForm
+
+
+def login_view(request):
+    next = request.GET.get('next')
+    form = UserLoginForm(requst.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        if next:
+            return redirect(next)
+        return redirect('/')
+    
+    context = {
+        'form': form,  
+    }
+    return render(request, "login.html", context)
